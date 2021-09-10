a2b09e03bd02129ffb8cfab557396ad5681bd263
DevProjekts <88111594+DevProjekts@users.noreply.github.com>
2021-09-10 19:12:13 +0200

    Update cannon game.py

--- cannon game.py
+++ cannon game.py
@@ -1,37 +1,29 @@
-"""Cannon, hitting targets with projectiles.
-Exercises
-1. Keep score by counting target hits.
-2. Vary the effect of gravity.
-3. Apply gravity to the targets.
-4. Change the speed of the ball.
-"""
+#Cannon, hitting targets with projectiles.
+
 
 from random import randrange
 from turtle import *
-
+#The link to the module https://pypi.org/project/freegames/
 from freegames import vector
 
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
-
+#This part of code is responding to tap the screen
 def tap(x, y):
-    "Respond to screen tap."
     if not inside(ball):
         ball.x = -199
         ball.y = -199
         speed.x = (x + 200) / 25
         speed.y = (y + 200) / 25
 
-
+#We are returning the True if xy is within screen
 def inside(xy):
-    "Return True if xy within screen."
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
-
+#Drawing the ball and the targets
 def draw():
-    "Draw ball and targets."
     clear()
 
     for target in targets:
@@ -44,9 +36,8 @@ def draw():
 
     update()
 
-
+#We are creating moving ball and targets
 def move():
-    "Move ball and targets."
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
